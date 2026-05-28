f0a5e5a956c24e5fce7bae655b55b3dcd7d42c82
Brinda1612 <brindaponkiya1000@gmail.com>
2026-05-28 17:23:35 +0530

    chore: revert central server API URL to production endpoint in constants and config

--- netranext_client/constants.py
+++ netranext_client/constants.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, NetraNext and contributors
 # For license information, please see license.txt
+# production step
+DEFAULT_CENTRAL_SERVER_URL = "https://netranext.m.frappe.cloud"
 
-# DEFAULT_CENTRAL_SERVER_URL = "https://netranext.m.frappe.cloud"
-
-DEFAULT_CENTRAL_SERVER_URL = "http://netranext-service.local:8000"
+# DEFAULT_CENTRAL_SERVER_URL = "http://netranext-service.local:8000"
 
